0829a81c81227bce773734564474f56cddedd550
iftekar2 <iftekar.hossain123@gmail.com>
2023-12-03 12:49:50 -0500

    Removing file and fixing navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import Home from "./Pages/Home";
-import styled from "styled-components";
-import NavBar from "./Components/Nav";
 
 function App() {
-  return (
-    <AppPage>
-      <NavBar />
-      <Home />
-    </AppPage>
-  );
+  return <div>App</div>;
 }
-
-const AppPage = styled.div`
-  background-color: #fff7f4;
-`;
 
 export default App;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,28 +1,5 @@
-//import Nav from "../Components/Nav";
-import styled from "styled-components";
-import HomeComponents from "../Components/HomeComponents";
-import About from "../Components/About";
-import Favorite from "../Components/Favorite";
-import FAQ from "../Components/FAQ";
-import Footer from "../Components/Footer";
-
 function Home() {
-  return (
-    <div className="HomePage">
-      <HomePage>
-        <HomeComponents />
-        <About />
-        <Favorite />
-        <FAQ />
-        <Footer />
-      </HomePage>
-    </div>
-  );
+  return <div>Home</div>;
 }
-
-const HomePage = styled.div`
-  margin-left: 2rem;
-  margin-right: 2rem;
-`;
 
 export default Home;
