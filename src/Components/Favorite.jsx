44fbeda6b7be30b782376f5b6a95fd47b6b1f8e8
iftekar2 <iftekar.hossain123@gmail.com>
2023-12-02 20:38:29 -0500

    Adding elements to Categories.jsx component

--- src/Components/Favorite.jsx
+++ src/Components/Favorite.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import Categories from "./Categories";
 
 function Favorite() {
   const [popular, setPopular] = useState([]);
@@ -29,23 +30,7 @@ function Favorite() {
 
         <div className="favoriteComponents">
           <div className="countries">
-            <Countries>
-              <Italian>
-                <img src="https://cdn-icons-png.flaticon.com/512/4624/4624250.png" />
-              </Italian>
-              <American>
-                <img src="https://cdn-icons-png.flaticon.com/512/2505/2505032.png" />
-              </American>
-              <Indian>
-                <img src="https://cdn-icons-png.flaticon.com/512/4727/4727322.png" />
-              </Indian>
-              <Chinese>
-                <img src="https://cdn-icons-png.flaticon.com/512/1623/1623786.png" />
-              </Chinese>
-              <Japanese>
-                <img src="https://cdn-icons-png.flaticon.com/512/12391/12391176.png" />
-              </Japanese>
-            </Countries>
+            <Categories />
           </div>
           <SearchBar>
             <input type="text" placeholder="Find Food" />
@@ -91,139 +76,139 @@ const FavoriteTitle = styled.div`
   }
 `;
 
-const Countries = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  gap: 2rem;
-`;
+// const Countries = styled.div`
+//   display: flex;
+//   flex-wrap: wrap;
+//   justify-content: center;
+//   align-items: center;
+//   gap: 2rem;
+// `;
 
-const Italian = styled.div`
-  background: #053161;
-  width: 10rem;
-  height: 6rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 5%;
-  cursor: pointer;
+// const Italian = styled.div`
+//   background: #053161;
+//   width: 10rem;
+//   height: 6rem;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 5%;
+//   cursor: pointer;
 
-  img {
-    height: 6rem;
-    width: 6rem;
-  }
+//   img {
+//     height: 6rem;
+//     width: 6rem;
+//   }
 
-  @media (max-width: 950px) {
-    width: 8rem;
-    height: 4rem;
+//   @media (max-width: 950px) {
+//     width: 8rem;
+//     height: 4rem;
 
-    img {
-      height: 4rem;
-      width: 4rem;
-    }
-  }
-`;
-const American = styled.div`
-  background: #61bfad;
-  width: 10rem;
-  height: 6rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 5%;
-  cursor: pointer;
+//     img {
+//       height: 4rem;
+//       width: 4rem;
+//     }
+//   }
+// `;
+// const American = styled.div`
+//   background: #61bfad;
+//   width: 10rem;
+//   height: 6rem;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 5%;
+//   cursor: pointer;
 
-  img {
-    height: 6rem;
-    width: 6rem;
-  }
+//   img {
+//     height: 6rem;
+//     width: 6rem;
+//   }
 
-  @media (max-width: 950px) {
-    width: 8rem;
-    height: 4rem;
+//   @media (max-width: 950px) {
+//     width: 8rem;
+//     height: 4rem;
 
-    img {
-      height: 4rem;
-      width: 4rem;
-    }
-  }
-`;
-const Indian = styled.div`
-  background: #0bbcd6;
-  width: 10rem;
-  height: 6rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 5%;
-  cursor: pointer;
+//     img {
+//       height: 4rem;
+//       width: 4rem;
+//     }
+//   }
+// `;
+// const Indian = styled.div`
+//   background: #0bbcd6;
+//   width: 10rem;
+//   height: 6rem;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 5%;
+//   cursor: pointer;
 
-  img {
-    height: 6rem;
-    width: 6rem;
-  }
+//   img {
+//     height: 6rem;
+//     width: 6rem;
+//   }
 
-  @media (max-width: 950px) {
-    width: 8rem;
-    height: 4rem;
+//   @media (max-width: 950px) {
+//     width: 8rem;
+//     height: 4rem;
 
-    img {
-      height: 4rem;
-      width: 4rem;
-    }
-  }
-`;
-const Chinese = styled.div`
-  background: #178e96;
-  width: 10rem;
-  height: 6rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 5%;
-  cursor: pointer;
+//     img {
+//       height: 4rem;
+//       width: 4rem;
+//     }
+//   }
+// `;
+// const Chinese = styled.div`
+//   background: #178e96;
+//   width: 10rem;
+//   height: 6rem;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 5%;
+//   cursor: pointer;
 
-  img {
-    height: 6rem;
-    width: 6rem;
-  }
+//   img {
+//     height: 6rem;
+//     width: 6rem;
+//   }
 
-  @media (max-width: 950px) {
-    width: 8rem;
-    height: 4rem;
+//   @media (max-width: 950px) {
+//     width: 8rem;
+//     height: 4rem;
 
-    img {
-      height: 4rem;
-      width: 4rem;
-    }
-  }
-`;
-const Japanese = styled.div`
-  background: #ef3e4a;
-  width: 10rem;
-  height: 6rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10%;
-  cursor: pointer;
+//     img {
+//       height: 4rem;
+//       width: 4rem;
+//     }
+//   }
+// `;
+// const Japanese = styled.div`
+//   background: #ef3e4a;
+//   width: 10rem;
+//   height: 6rem;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 10%;
+//   cursor: pointer;
 
-  img {
-    height: 6rem;
-    width: 6rem;
-  }
+//   img {
+//     height: 6rem;
+//     width: 6rem;
+//   }
 
-  @media (max-width: 950px) {
-    width: 8rem;
-    height: 4rem;
+//   @media (max-width: 950px) {
+//     width: 8rem;
+//     height: 4rem;
 
-    img {
-      height: 4rem;
-      width: 4rem;
-    }
-  }
-`;
+//     img {
+//       height: 4rem;
+//       width: 4rem;
+//     }
+//   }
+// `;
 
 const SearchBar = styled.div`
   display: flex;
