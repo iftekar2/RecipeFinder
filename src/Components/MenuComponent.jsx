18919b06b4959d360804f1465aa54976ff8cab6a
iftekar2 <iftekar.hossain123@gmail.com>
2023-12-10 21:20:15 -0500

    Creating MenuComponent.jsx component

---
+++ src/Components/MenuComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MenuComponent() {
+  return <div>MenuComponent</div>;
+}
+
+export default MenuComponent;
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import styled from "styled-components";
+import MenuComponent from "../Components/MenuComponent";
 
 function Menu() {
-  return <div>Menu</div>;
+  return (
+    <MenuSection>
+      <MenuComponent />
+    </MenuSection>
+  );
 }
+
+const MenuSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Menu;
